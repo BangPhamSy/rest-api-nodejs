4f2fca434dbb01869fd6a5e5493b9012948f095c
BangPS <bang.pham@icomm.vn>
2021-05-10 22:43:18 +0700

    Tích hợp swagger cho dự án

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -4,15 +4,19 @@ import { Router } from "express";
 import AuthController from "./auth.controller";
 
 export default class AuthRoute implements Route {
-    public path = '/api/auth';
-    public router = Router(); 
-    public authController = new AuthController();
+  public path = "/api/v1/auth";
+  public router = Router();
+  public authController = new AuthController();
 
   constructor() {
     this.initializeRoutes();
   }
   private initializeRoutes() {
-    this.router.post(this.path,this.authController.login); //POST: http://localhost:5000/api/auth
-    this.router.get(this.path,authMiddleware,this.authController.getCurrentLoginUser); //GET: http://localhost:5000/api/auth
+    this.router.post(this.path, this.authController.login); //POST: http://localhost:5000/api/auth
+    this.router.get(
+      this.path,
+      authMiddleware,
+      this.authController.getCurrentLoginUser
+    ); //GET: http://localhost:5000/api/auth
   }
 }
