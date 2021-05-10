172b466d9824231d6aaecc1919c71731742ee520
DESKTOP-HTIOQVL\HL2020 <bang.pham@icomm.vn>
2021-05-10 17:01:31 +0700

    Thêm api tạo profile,get profile

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -1,5 +1,6 @@
 import UserController from "./users.controller";
 import UserRoute from "./users.route";
 import UserSchema from './users.model';
+import IUser from "./users.interface";
 
-export {UserController, UserRoute, UserSchema} 
+export {UserController, UserRoute, UserSchema, IUser} 
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import { IndexRoute } from '@modules/index';
 import { UserRoute } from '@modules/users';
 import { validateEnv } from '@core/utils';
 import { AuthRoute } from '@modules/auth';
+import { ProfileRoute } from '@modules/profile';
 
 validateEnv();
 const routes = [
     new IndexRoute(),
     new UserRoute(),
-    new AuthRoute()
+    new AuthRoute(),
+    new ProfileRoute()
 ];
 const app = new App(routes);
 app.listen();
